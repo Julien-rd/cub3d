dabea6d0c7a2ee803b9e7c1270494b3d7a9b18d0
Vesta Manuyko <Vesta.manuyko@gmail.com>
2026-04-15 15:13:18 +0200

    updated tester with more cases

--- tests/tester.c
+++ tests/tester.c
@@ -12,7 +12,7 @@ t_result	run_cub3d(char *file)
 	return (res);
 }
 
-Test(cub3d, empty_file)
+Test(invalid, empty_file)
 {
 	t_result res = run_cub3d("./maps/invalid/empty.cub");
 
@@ -20,7 +20,7 @@ Test(cub3d, empty_file)
 	cr_assert_str_eq(res.stderr, "Error\nEmpty file !\n");
 }
 
-Test(cub3d, no_map)
+Test(invalid, no_map)
 {
 	t_result res = run_cub3d("./maps/invalid/no_map.cub");
 
@@ -28,7 +28,7 @@ Test(cub3d, no_map)
 	cr_assert_str_eq(res.stderr, "Error\nNo map found!\n");
 }
 
-Test(cub3d, wrong_extension)
+Test(cub3d, wrong_extension_cubb)
 {
 	t_result res = run_cub3d("./maps/invalid/invalid_extension.cubbb");
 
@@ -36,10 +36,133 @@ Test(cub3d, wrong_extension)
 	cr_assert_str_eq(res.stderr, "Error\ninvalid input format\n");
 }
 
-Test(cub3d, missing_texture)
+
+Test(invalid, no_args)
+{
+	t_result res = run_cub3d(0);
+	
+	cr_assert_eq(res.exit_code, 1);
+	cr_assert_str_eq(res.stderr, "Error\nInvalid number of arguments!\n");
+}
+
+Test(invalid, empty_arg)
+{
+	t_result res = run_cub3d("");
+	
+	cr_assert_eq(res.exit_code, 1);
+	cr_assert_str_eq(res.stderr, "Error\ninvalid input format\n");
+}
+
+Test(invalid, hidden_file_in_folder)
+{
+	t_result res = run_cub3d("./maps/invalid/.cub");
+	
+	cr_assert_eq(res.exit_code, 1);
+	cr_assert_str_eq(res.stderr, "Error\ninvalid input format\n");
+}
+
+Test(invalid, hidden_file)
+{
+	t_result res = run_cub3d(".cub");
+	
+	cr_assert_eq(res.exit_code, 1);
+	cr_assert_str_eq(res.stderr, "Error\ninvalid input format\n");
+}
+//to be modified to correct err_message, when bug is fixed
+Test(invalid, empty_xpm_file)
+{
+	t_result res = run_cub3d("./maps/invalid/empty_xpm_file.cub");
+	
+	cr_assert_eq(res.exit_code, 1);
+	cr_assert_str_eq(res.stderr, "Error\nFailed to load north texture\n");
+}
+
+//This test is to be added after the bug with hidden files is fixed.
+
+// Test(invalid, linked_hidden_file_xpm)
+// {
+// 	t_result res = run_cub3d("link_xpm_file.cub");
+
+// 	cr_assert_eq(res.exit_code, 1);
+// 	cr_assert_str_eq(res.stderr, "Error\nInvalid input!\n");
+// }
+
+Test(invalid, invalid_char_in_map)
+{
+	t_result res = run_cub3d("./maps/invalid/invalid_char_in_map.cub");
+	
+	cr_assert_eq(res.exit_code, 1);
+	cr_assert_str_eq(res.stderr, "Error\nInvalid input!\n");
+}
+
+Test(invalid, wrong_extension_txt)
+{
+	t_result res = run_cub3d("./maps/invalid/invalid_extension.txt");
+	
+	cr_assert_eq(res.exit_code, 1);
+	cr_assert_str_eq(res.stderr, "Error\ninvalid input format\n");
+}
+
+Test(invalid, misplaced_info)
+{
+	t_result res = run_cub3d("./maps/invalid/misplaced_info.cub");
+	
+	cr_assert_eq(res.exit_code, 1);
+	cr_assert_str_eq(res.stderr, "Error\nInvalid input!\n");
+}
+
+Test(invalid, missing_texture)
 {
 	t_result res = run_cub3d("./maps/invalid/missing_texture.cub");
 
 	cr_assert_eq(res.exit_code, 1);
 	cr_assert_str_eq(res.stderr, "Error\nNo path found!\n");
+}
+
+Test(invalid, only_map)
+{
+	t_result res = run_cub3d("./maps/invalid/only_map.cub");
+	
+cr_assert_eq(res.exit_code, 1);
+cr_assert_str_eq(res.stderr, "Error\nInvalid input!\n");
+}
+
+Test(invalid, player_out_of_map)
+{
+	t_result res = run_cub3d("./maps/invalid/player_out_of_map.cub");
+	
+cr_assert_eq(res.exit_code, 1);
+cr_assert_str_eq(res.stderr, "Error\nInvalid input!\n");
+}
+
+Test(invalid, random_content)
+{
+	t_result res = run_cub3d("./maps/invalid/random_content.cub");
+	
+cr_assert_eq(res.exit_code, 1);
+cr_assert_str_eq(res.stderr, "Error\nNo map found!\n");
+}
+
+Test(invalid, space_in_colour)
+{
+	t_result res = run_cub3d("./maps/invalid/space_in_color.cub");
+	
+cr_assert_eq(res.exit_code, 1);
+cr_assert_str_eq(res.stderr, "Error\nInvalid input!\n");
+}
+
+Test(invalid, space_in_map)
+{
+	t_result res = run_cub3d("./maps/invalid/space_in_map.cub");
+	
+cr_assert_eq(res.exit_code, 1);
+cr_assert_str_eq(res.stderr, "Error\nEmpty line in map!\n");
+}
+
+Test(invalid, multiple_maps)
+{
+	t_result res = run_cub3d("./maps/invalid/multiple_maps.cub");
+	
+cr_assert_eq(res.exit_code, 1);
+cr_assert_str_eq(res.stderr, "Error\nEmpty line in map!\n");
 }
